6bba512a350f06a30ef1d8b6e0fe05b60625a8a2
Gyeongtae Kim <codingpelican@gmail.com>
2022-12-08 19:05:59 +0900

    implement simplest sql compiler

--- main.c
+++ main.c
@@ -9,15 +9,32 @@
         __builtin_trap();                                        \
     }                                                            \
 
-enum {
-    ALLOC_SIZE = 64
-};
-
 typedef struct {
     char* Buffer;
     size_t BufferLength;
     ssize_t InputLength;
 } InputBuffer;
+
+typedef enum {
+    META_COMMAND_SUCCESS,
+    META_COMMAND_UNRECOGNIZED_COMMAND
+} eMetaCommandResult;
+
+typedef enum {
+    PREPARE_SUCCESS,
+    PREPARE_UNRECOGNIZED_STATEMENT
+} ePrepareResult;
+
+typedef enum {
+    STATEMENT_INSERT,
+    STATEMENT_SELECT
+} eStatementType;
+
+typedef struct {
+    eStatementType Type;
+} Statement;
+
+const int ALLOC_SIZE = 64;
 
 InputBuffer* NewInputBuffer() {
     InputBuffer* inputBuffer = (InputBuffer*)malloc(sizeof(InputBuffer));
@@ -48,7 +65,7 @@ ssize_t GetLine(char** lineptr, size_t* n, FILE* stream) {
             return -1;
         }
         len += strlen(cur);
-        if ((*lineptr)[len - 1] != '\n') { // 개행 문자 확인
+        if ((*lineptr)[len - 1] != '\n') { // * 개행 문자 확인
             char* new = (char*)realloc(*lineptr, *n += ALLOC_SIZE);
             if (!new) {
                 return -1;
@@ -68,7 +85,7 @@ void ReadLine(InputBuffer* inputBuffer) {
         printf("Error reading input\n");
         exit(EXIT_FAILURE); // NOLINT
     }
-    //// Ignore trailing newline
+    // * Ignore trailing newline
     // inputBuffer->InputLength = bytesRead - 1;
     // inputBuffer->Buffer[bytesRead - 1] = 0;
 }
@@ -78,18 +95,65 @@ void CloseInputBuffer(InputBuffer* inputBuffer) {
     free(inputBuffer);
 }
 
+eMetaCommandResult DoMetaCommand(InputBuffer* inputBuffer) {
+    if (strcmp(inputBuffer->Buffer, ".exit") == 0) {
+        exit(EXIT_SUCCESS); // NOLINT
+    } else {
+        return META_COMMAND_UNRECOGNIZED_COMMAND;
+    }
+}
+
+ePrepareResult PrepareStatement(InputBuffer* inputBuffer, Statement* statement) {
+    if (strncmp(inputBuffer->Buffer, "insert", 6) == 0) { // NOLINT
+        statement->Type = STATEMENT_INSERT;
+        return PREPARE_SUCCESS;
+    }
+    if (strcmp(inputBuffer->Buffer, "select") == 0) {
+        statement->Type = STATEMENT_SELECT;
+        return PREPARE_SUCCESS;
+    }
+
+    return PREPARE_UNRECOGNIZED_STATEMENT;
+}
+
+void ExecuteStatement(Statement* statement) {
+    switch (statement->Type) {
+    case (STATEMENT_INSERT):
+        printf("This is where we would do an insert.\n");
+        break;
+    case (STATEMENT_SELECT):
+        printf("This is where we would do a select.\n");
+        break;
+    }
+}
+
 int main(int argc, char const* argv[]) {
     InputBuffer* inputBuffer = NewInputBuffer();
     while (true) {
         PrintPrompt();
         ReadLine(inputBuffer);
 
-        if (strcmp(inputBuffer->Buffer, ".exit") == 0) {
-            CloseInputBuffer(inputBuffer);
-            exit(EXIT_SUCCESS); // NOLINT
-        } else {
-            printf("Unrecognized command '%s'.\n", inputBuffer->Buffer);
+        if (inputBuffer->Buffer[0] == '.') {
+            switch (DoMetaCommand(inputBuffer)) {
+            case (META_COMMAND_SUCCESS):
+                continue;
+            case (META_COMMAND_UNRECOGNIZED_COMMAND):
+                printf("Unrecognized command '%s'\n", inputBuffer->Buffer);
+                continue;
+            }
         }
+
+        Statement statement;
+        switch (PrepareStatement(inputBuffer, &statement)) {
+        case (PREPARE_SUCCESS):
+            break;
+        case (PREPARE_UNRECOGNIZED_STATEMENT):
+            printf("Unrecognized command '%s'\n", inputBuffer->Buffer);
+            continue;
+        }
+
+        ExecuteStatement(&statement);
+        printf("Executed.\n");
     }
     return 0;
 }
